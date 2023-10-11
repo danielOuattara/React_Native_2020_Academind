0b6aa770237602fdb11322c3f19e1ffef84a79c5
danielOuattara <daniel.ouattara@gmx.com>
2023-10-11 19:42:08 +0200

    04 Guess game start v2 + added Screens

---
+++ 04_Deeper_In_Mini-Game/v2/App.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { StartGameScreen } from "./screens";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ 04_Deeper_In_Mini-Game/v2/screens/StartGameScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import React from "react";
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput placeholder="Enter a number to guess" />
+      {/* BUTTONS HERE */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
