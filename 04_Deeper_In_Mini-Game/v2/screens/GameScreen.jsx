d27d9241aef7e5360a1a0931bff9b57fd62bb349
danielOuattara <daniel.ouattara@gmx.com>
2023-10-12 21:04:49 +0200

    04-60 Starting styling GameScreen

--- 04_Deeper_In_Mini-Game/v2/screens/GameScreen.jsx
+++ 04_Deeper_In_Mini-Game/v2/screens/GameScreen.jsx
@@ -3,10 +3,23 @@ import React from "react";
 
 export default function GameScreen() {
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>GameScreen</Text>
+      <View>
+        <Text> Higher OR Lower</Text>
+      </View>
+      <View>
+        <Text>Log Rounds details</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+    borderWidth: 4,
+    borderColor: "green",
+  },
+});
