32211e5b312dff1c3492821a9890bb7582e6705a
danielOuattara <daniel.ouattara@gmx.com>
2024-01-08 23:01:09 +0100

    04 v2 Using & Styling Nested Text

--- 04_Deeper_In_Mini-Game/v2/screens/GameOverScreen.jsx
+++ 04_Deeper_In_Mini-Game/v2/screens/GameOverScreen.jsx
@@ -1,5 +1,5 @@
 import { Image, StyleSheet, Text, View } from "react-native";
-import { Title } from "../components";
+import { PrimaryButton, Title } from "../components";
 import { Colors } from "../constants";
 
 export default function GameOverScreen() {
@@ -12,9 +12,13 @@ export default function GameOverScreen() {
           style={styles.image}
         />
       </View>
-      <View>
-        <Text>Your phone made X rounds to guess the value of Y</Text>
-      </View>
+
+      <Text style={styles.summaryText}>
+        Your phone made <Text style={styles.highlight}> X </Text>
+        rounds to guess the value of <Text style={styles.highlight}> Y </Text>
+      </Text>
+
+      <PrimaryButton>Start New game ?</PrimaryButton>
     </View>
   );
 }
@@ -22,22 +26,31 @@ export default function GameOverScreen() {
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    margin: 24,
+    margin: 20,
     justifyContent: "center",
     alignItems: "center",
   },
   imageContainer: {
-    borderRadius: 150,
+    borderRadius: 175,
     overflow: "hidden",
-    width: 300,
-    height: 300,
+    width: 250,
+    height: 250,
     borderWidth: 3,
     borderColor: Colors.secondary500,
     margin: 36,
   },
-
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans-regular",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
