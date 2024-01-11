3392061d4af3ac1c1dc77ecd87b8466892f76bdd
danielOuattara <daniel.ouattara@gmx.com>
2024-01-11 16:33:13 +0100

    05: 87 improving landscape UI for GameOverScreen

--- 05_Building_Adaptative_User_Interfaces/screens/GameOverScreen.jsx
+++ 05_Building_Adaptative_User_Interfaces/screens/GameOverScreen.jsx
@@ -1,34 +1,68 @@
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  // Dimensions,
+  useWindowDimensions,
+} from "react-native";
 import { PrimaryButton, Title } from "../components";
 import { Colors } from "../constants";
 
 export default function GameOverScreen(props) {
+  const { width, height, scale, fontScale } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  // if (width < 340) {
+  //   imageSize = 150;
+  // }
+
+  // if (height < 400) {
+  //   imageSize = 80;
+  // }
+
+  if (height < width) {
+    imageSize = 150;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
+
   return (
-    <View style={styles.rootContainer}>
-      <Title>Game over</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("./../assets/images/success.png")}
-          style={styles.image}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>Game over</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            source={require("./../assets/images/success.png")}
+            style={styles.image}
+          />
+        </View>
+
+        <Text style={styles.summaryText}>
+          Your phone made{" "}
+          <Text style={styles.highlight}> {props.counter} </Text>
+          rounds to guess the value of{" "}
+          <Text style={styles.highlight}> {props.userNumber} </Text>
+        </Text>
+
+        <PrimaryButton pressAction={props.startNewGame}>
+          Start New game ?
+        </PrimaryButton>
       </View>
-
-      <Text style={styles.summaryText}>
-        Your phone made <Text style={styles.highlight}> {props.counter} </Text>
-        rounds to guess the value of{" "}
-        <Text style={styles.highlight}> {props.userNumber} </Text>
-      </Text>
-
-      <PrimaryButton pressAction={props.startNewGame}>
-        Start New game ?
-      </PrimaryButton>
-    </View>
+    </ScrollView>
   );
 }
 
-const deviceWidth = Dimensions.get("window").width;
+// const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
+  screen: { flex: 1 },
   rootContainer: {
     flex: 1,
     margin: 20,
@@ -36,10 +70,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    borderRadius: 175,
     overflow: "hidden",
-    width: deviceWidth < 340 ? 180 : 260,
-    height: deviceWidth < 340 ? 180 : 260,
+    // borderRadius: deviceWidth < 340 ? 150 : 300,
+    // width: deviceWidth < 340 ? 180 : 260,
+    // height: deviceWidth < 340 ? 180 : 260,
     borderWidth: 3,
     borderColor: Colors.secondary500,
     margin: 36,
